c789a67cac3cb8ccc7783d834b846bde9619f3f5
Mohammed <mohammed.ansari@sjsu.edu>
2024-04-26 16:46:32 -0700

    test

--- BST/src/TreeException.java
+++ BST/src/TreeException.java
@@ -1,7 +1,8 @@
-public class TreeException extends FamilyTree
+public class TreeException extends RuntimeException
 {
 	public TreeException(String s)
 	{
+		super(s);
 
 		// Pass s into a superclass ctor.
 	}
